f2c435c8b6b35f8b12ffe44455bdbe74c9307e3b
stellarsailor <dev.stellarsailor@gmail.com>
2021-03-25 12:03:49 -0400

    Animation draft

---
+++ src/components/StarDestroyer/StarDestroyer.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import { motion } from "framer-motion"
+
+function StarDestroyer() {
+  return (
+      <Container
+        initial={{ opacity: 0.8, x: '50vw', y: '50vh' }}
+        animate={{ opacity: 1, x: '30vw', y: '30vh' }}
+        transition={{ delay: 0.2, duration: 5 }}
+      >
+        <Image />
+      </Container>
+  )
+}
+
+const Container = styled(motion.div)`
+  bottom: 0;
+  width: 80%;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Image = styled.img.attrs({
+  src: '/images/stardestroyer.png'
+})`
+  position: absolute;
+  width: 100%;
+  height: auto;
+`
+
+export default StarDestroyer
