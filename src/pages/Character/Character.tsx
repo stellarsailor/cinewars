3aa531bfc59281d526e3de4d214ef73033fc8776
stellarsailor <dev.stellarsailor@gmail.com>
2021-03-24 22:07:40 -0400

    Tested basic fetching routes

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react"
+import { useHistory, useParams } from 'react-router-dom'
+
 export type CharacterProps = {}
 
 function Character({}: CharacterProps) {
-  return <div>Character</div>
+  let params: any = useParams()
+  const history = useHistory()
+
+  const [ character, setCharacter ] = useState<any>(null)
+
+  useEffect(() => {
+    const initialFetch = async () => {
+      let url = `https://swapi.dev/api/people/${params.id}`
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+  
+        setCharacter(data)
+      } catch (error) {
+        
+      }
+    }
+
+    initialFetch()
+  },[])
+
+  return (
+    <div>
+      {character && 
+        <div>
+          {character.name} {character.height}
+        </div>}
+      <div onClick={() => history.goBack()}>Back</div>
+    </div>
+  )
 }
 
 export default Character
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
 export type MainProps = {}
 
 function Main({}: MainProps) {
-  return <div>Main</div>
+
+  const [ page, setPage ] = useState(1)
+  const [ loading, setLoading ] = useState(false)
+  const [ list, setList ] = useState<Array<any>>([])
+  
+  useEffect(() => {
+    const initialFetch = async () => {
+      setLoading(true)
+      let url = `http://swapi.dev/api/people/?page=${page}`;
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data)
+        setList(data.results)
+        setLoading(false)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    initialFetch()
+  },[page])
+
+  if(loading) {
+    return <div>loading..</div>
+  }
+
+  return (
+    <div>
+      {list.map( (v, index) => (
+        <Link to={`/character/${(page-1) * 10 + (index+1)}`}>
+          <div onClick={() => console.log( (page-1) * 10 + (index+1) )}>
+            {v.name}
+          </div>
+        </Link>
+      ))}
+      <div>
+        <div onClick={() => setPage(page - 1)}>prev</div>
+        <div onClick={() => setPage(page + 1)}>next</div>
+      </div>
+    </div>
+  )
 }
 
 export default Main
